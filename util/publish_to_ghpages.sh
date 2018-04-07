253e6f5ace96f3b3e96dd5bb5cc37377de95d0b6
Denis C de Azevedo <denis.azevedo@gmail.com>
2018-04-07 18:55:06 -0300

    add theme and basic info

---
+++ util/publish_to_ghpages.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+UPSTREAM=${1:-origin}
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public $UPSTREAM/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+# TODO add the date to the commit msg
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+#git push origin gh-pages
---
+++ util/serve.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+hugo server -b http://localhost:1313/
