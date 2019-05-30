0f1a2e422f2c6ef1e1d55e1af51d907f07c81182
Denis C de Azevedo <denis.azevedo@gmail.com>
2019-05-29 21:44:21 -0300

    2019-05-29 21:44:21 publish to gh-pages (script)

--- js/google_map.js
+++ js/google_map.js
@@ -1,74 +1,81 @@
+(function () {
+'use strict';
+
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var myLatlng = new google.maps.LatLng(29.6864011,-82.3899576); // Gainesville (half-way Georgia x Florida)
-    
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 6,
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var myLatlng = new google.maps.LatLng(29.6864011,-82.3899576); // Gainesville (half-way Georgia x Florida)
+  
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 6,
+    // The latitude and longitude to center the map (always required)
+    center: myLatlng,
+    // How you would like to style the map. 
+    scrollwheel: false,
+    styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
+  };
 
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
+  // Get the HTML DOM element that will contain your map 
+  // We are using a div with id="map" seen below in the <body>
+  var mapElement = document.getElementById('map');
 
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    // Para aumento de performance, o geolocation já está definido para os endereços da ACP
-    // placeMarkersFromGeolocationSearch(map);
-    
-    placeMarkersFromGeolocationArray(map);
+  // Create the Google Map using out element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
+  
+  // Para aumento de performance, o geolocation já está definido para os endereços da ACP
+  // placeMarkersFromGeolocationSearch(map);
+  
+  placeMarkersFromGeolocationArray(map);
 }
 
 function placeMarkersFromGeolocationArray(map) {
-    var addresses = [
-        {
-            name: 'Georgia, USA',
-            location: {
-                lat: 32.165622, lng: -82.900075
-            }
-        }, {
-            name: 'Florida, USA',
-            location: {
-                lat: 27.664827, lng: -81.515754
-            }
-        },
-    ];
-    for (var x = 0; x < addresses.length; x++) {
-        var loc = addresses[x].location;
-        var latlng = new google.maps.LatLng(loc.lat, loc.lng);
-        new google.maps.Marker({
-            position: latlng,
-            map: map
-            // icon: 'images/loc.png'
-        });
-    }
+  var addresses = [
+    {
+      name: 'Georgia, USA',
+      location: {
+          lat: 32.165622, lng: -82.900075
+      }
+    }, {
+      name: 'Florida, USA',
+      location: {
+          lat: 27.664827, lng: -81.515754
+      }
+    },
+  ];
+  for (var x = 0; x < addresses.length; x++) {
+    var loc = addresses[x].location;
+    var latlng = new google.maps.LatLng(loc.lat, loc.lng);
+    new google.maps.Marker({
+      position: latlng,
+      map: map
+      // icon: 'images/loc.png'
+    });
+  }
 }
 
+/*
 function placeMarkersFromGeolocationSearch(map) {
-    var addresses = ['Georgia, USA', 'Florida, USA'];
+  var addresses = ['Georgia, USA', 'Florida, USA'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('//maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location;
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map
-                // icon: 'images/loc.png'
-            });
-        });
-    }
+  for (var x = 0; x < addresses.length; x++) {
+    $.getJSON('//maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+      var p = data.results[0].geometry.location;
+      var latlng = new google.maps.LatLng(p.lat, p.lng);
+    new google.maps.Marker({
+        position: latlng,
+        map: map
+        // icon: 'images/loc.png'
+      });
+    });
+  }
 }
+*/
 
-google.maps.event.addDomListener(window, 'load', init);
+// $(window).load(function() {
+  google.maps.event.addDomListener(window, 'load', init);
+// });
+
+}());
